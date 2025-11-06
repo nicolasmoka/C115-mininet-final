074de0a18ccbc27e623a991911e7131313c455d1
nicolasmoka <nicolasmkz2004@outlook.com>
2025-11-06 19:36:06 -0300

    Arrumando topologia

--- exercicio2.py
+++ exercicio2.py
@@ -6,28 +6,31 @@ from mininet.link import TCLink
 
 class MyTopo(Topo):
     def build(self):
+        # Initialize topology
+        Topo.__init__( self )
+
         # Hosts with standardized MACs
         h1 = self.addHost('h1', mac='00:00:00:00:00:01')
         h2 = self.addHost('h2', mac='00:00:00:00:00:02')
         h3 = self.addHost('h3', mac='00:00:00:00:00:03')
         h4 = self.addHost('h4', mac='00:00:00:00:00:04')
+        h5 = self.addHost('h5', mac='00:00:00:00:00:05')
 
         # Switches
         s1 = self.addSwitch('s1')
         s2 = self.addSwitch('s2')
         s3 = self.addSwitch('s3')
-        s4 = self.addSwitch('s4')
 
         # Conctions between switches
         self.addLink(s1, s2)
-        self.addLink(s1, s3)
-        self.addLink(s1, s4)
+        self.addLink(s2, s3)
 
         # Conctions between switches and hosts
-        self.addLink(s2, h1)
-        self.addLink(s2, h2)
-        self.addLink(s3, h3)
-        self.addLink(s4, h4)
+        self.addLink(s1, h1)
+        self.addLink(s1, h2)
+        self.addLink(s2, h3)
+        self.addLink(s3, h4)
+        self.addLink(s3, h5)
 
 if __name__ == '__main__':
     topo = MyTopo()
